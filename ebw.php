ab389057f36824826a1e782a8dd2089a88e639d8
TSStudio <TSS@mail.tmysam.top>
2019-09-28 16:08:16 +0800

    Edited

--- ebw.php
+++ ebw.php
@@ -12,75 +12,44 @@ include './include/server-info.php';
 $lang=$_SESSION['language'];
 include './include/'.$lang.'.php';
 ?>
-<html>
-    <head>
-        <link href="css/style.css" rel="stylesheet">
-        <link href="css/all.css" rel="stylesheet">
-        <link href="css/pct.css" rel="stylesheet">
-        <title>Fortress II:Siege</title>
-        <script>
-            function buy(cass,cost){
-                if(confirm("<?=$ebb2?>"+cost+"<?=$ebpt?>")){
-                    window.location.href="pay.php?class="+cass;
-                }
-            }
-            function showhelp(){
-                alert("<?=$ebhm?>");
-            }
-        </script>
-        <?php
-                $db=new \mysqli($dbhost,$dbuser,$dbpawd,$dbname);
-                if ($db->connect_errno) {
-                    printf("Connect failed: %s\n", $db->connect_error);
-                    exit();
-                }
-                $sqll='SELECT * FROM point where Username=\''.strtolower($_SESSION['username']).'\'';
-                $rows = $db->query($sqll);
-                if (mysqli_num_rows($rows) < 1){
-                    header('Refresh:0;url=firsttime.php');
-                    die();
-                }
-                while($row = $rows->fetch_assoc()){
-                    $dbpoint=$row['Point'];
-                    $dblvlp=$row['lvlp'];
-                    $crtt=$row['crtt'];
-                }
-                if($dblvlp<500){
-                    $lvl=$ebl1;
-                    $next=500;
-                    $percent=$dblvlp/$next*100;
-                    $lvlb=1;
-                }else if($dblvlp<1500){
-                    $lvl=$ebl2;
-                    $next=1500;
-                    $percent=$dblvlp/$next*100;
-                    $lvlb=2;
-                }else if($dblvlp<2500){
-                    $lvl=$ebl3;
-                    $next=2500;
-                    $percent=$dblvlp/$next*100;
-                    $lvlb=3;
-                }else{
-                    $lvl=$ebl4;
-                    $next=2500;
-                    $percent=100;
-                    $lvlb=4;
-                }
-                ?>
-                <style type="text/css">
-                    .LVLB{
-                        width:<?php echo $percent.'%';?>;
-                    }
-                </style>
-        </head>
-            <body id="body">
-            <div id="Main">
-                <i class="iconfont icon-username"></i><?php echo $_SESSION['username']; ?><br>
-                <a href="index.php" class="button button-primary"><i class="iconfont icon-i-back"></i><?=$back?></a><br>
-                <?=$ebcp?>:(<a href="#" onclick="showhelp();">?</a>)：<?php echo $dbpoint;?>
-                <div class="LINEBOX"><div class="PERCENT LVLB"><nobr><center><?php echo $lvl.' '.$dblvlp.'/'.$next;?></center></nobr></div></div>
-    <br>
-    <?php
+<?php
+    $db=new \mysqli($dbhost,$dbuser,$dbpawd,$dbname);
+    if ($db->connect_errno) {
+        printf("Connect failed: %s\n", $db->connect_error);
+        exit();
+    }
+    $sqll='SELECT * FROM point where Username=\''.strtolower($_SESSION['username']).'\'';
+    $rows = $db->query($sqll);
+    if (mysqli_num_rows($rows) < 1){
+        header('Refresh:0;url=firsttime.php');
+        die();
+    }
+    while($row = $rows->fetch_assoc()){
+        $dbpoint=$row['Point'];
+        $dblvlp=$row['lvlp'];
+        $crtt=$row['crtt'];
+    }
+    if($dblvlp<500){
+        $lvl=$ebl1;
+        $next=500;
+        $percent=$dblvlp/$next*100;
+        $lvlb=1;
+    }else if($dblvlp<1500){
+        $lvl=$ebl2;
+        $next=1500;
+        $percent=$dblvlp/$next*100;
+        $lvlb=2;
+    }else if($dblvlp<2500){
+        $lvl=$ebl3;
+        $next=2500;
+        $percent=$dblvlp/$next*100;
+        $lvlb=3;
+    }else{
+        $lvl=$ebl4;
+        $next=2500;
+        $percent=100;
+        $lvlb=4;
+    }
     $rows=null;
     $row=null;
     $rows = $db->query('SELECT * FROM class where username=\''.strtolower($_SESSION['username']).'\'');//用sql语句获取数据
@@ -91,20 +60,39 @@ include './include/'.$lang.'.php';
         $rows = $db->query('SELECT * FROM class where username=\''.strtolower($_SESSION['username']).'\'');//用sql语句获取数据
     }
     while($row = $rows->fetch_assoc()){
-        echo '你曾经拥有';
+        $finaloutput="";
+        $finaloutput=$finaloutput.'你曾经拥有<br>';
         $times=1;
         while($times<=$classes){
             if($row[(string)$times]==1){
-                echo $truename[$times].",";
+                $finaloutput=$finaloutput.$truename[$times]." ";
             }
             $times++;
         }
+        $finaloutput=$finaloutput.'<br>这只是一个记录，未来这些职业的处理方式如何暂时未知。';
     }
-
 ?>
-</div>
-<div style="color:white;" class="copyright"><p>&nbsp;&nbsp;&nbsp;<?=$copyright?>&copy; 2014-<?php
-echo date('Y'); ?>.TS Studio <?=$alrr?> 吉ICP备17003700号</p></div>
-</body>
-<script defer src="./css/bgi.js"></script>
+<html>
+    <head>
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/all.css" rel="stylesheet">
+        <link href="css/pct.css" rel="stylesheet">
+        <title>Fortress II:Siege</title>
+        <style type="text/css">
+            .LVLB{
+                width: <?=$percent.'%'?>;
+            }
+        </style>
+    </head>
+    <body id="body" class="lazy">
+        <div class="paybox"><!--Main-->
+            <h1>Fortress II:Siege 已终止服务</h1>
+            <div class="LINEBOX"><div class="PERCENT LVLB"><nobr><center><?php echo $lvl.' '.$dblvlp.'/'.$next;?></center></nobr></div></div>
+            <div class="deti">
+                <?=$finaloutput?>
+            </div>
+        </div>
+        <div style="color:white;" class="copyright"><p>&nbsp;&nbsp;&nbsp;<?=$copyright?>&copy; 2014-<?php echo date('Y'); ?>.TS Studio <?=$alrr?> 吉ICP备17003700号</p></div>
+    </body>
+    <script src="./css/bgi.js"></script>
 </html>
