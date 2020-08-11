31638b0eecb76560198f94863e710f4254d79436
TSStudio <TSS@mail.tmysam.top>
2020-08-11 08:57:51 +0800

    复活ebw系统

--- ebw.php
+++ ebw.php
@@ -13,43 +13,88 @@ include './include/server-info.php';
 $lang=$_SESSION['language'];
 include './include/'.$lang.'.php';
 ?>
-<?php
-    $db=new \mysqli($dbhost,$dbuser,$dbpawd,$dbname);
-    if ($db->connect_errno) {
-        printf("Connect failed: %s\n", $db->connect_error);
-        exit();
+<html>
+    <head>
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/all.css" rel="stylesheet">
+        <link href="css/pct.css" rel="stylesheet">
+        <title>Fortress II:Siege</title>
+        <script>
+            function buy(cass,cost){
+                if(confirm("<?=$ebb2?>"+cost+"<?=$ebpt?>")){
+                    window.location.href="pay.php?class="+cass;
+                }
+            }
+            function showhelp(){
+                alert("<?=$ebhm?>");
+            }
+        </script>
+        <?php
+                $db=new \mysqli($dbhost,$dbuser,$dbpawd,$dbname);
+                if ($db->connect_errno) {
+                    printf("Connect failed: %s\n", $db->connect_error);
+                    exit();
+                }
+                $sqll='SELECT * FROM point where Username=\''.strtolower($_SESSION['username']).'\'';
+                $rows = $db->query($sqll);
+                if (mysqli_num_rows($rows) < 1){
+                    header('Refresh:0;url=firsttime.php');
+                    die();
+                }
+                while($row = $rows->fetch_assoc()){
+                    $dbpoint=$row['Point'];
+                    $dblvlp=$row['lvlp'];
+                    $crtt=$row['crtt'];
+                }
+                if($dblvlp<500){
+                    $lvl=$ebl1;
+                    $next=500;
+                    $percent=$dblvlp/$next*100;
+                    $lvlb=1;
+                }else if($dblvlp<1500){
+                    $lvl=$ebl2;
+                    $next=1500;
+                    $percent=$dblvlp/$next*100;
+                    $lvlb=2;
+                }else if($dblvlp<2500){
+                    $lvl=$ebl3;
+                    $next=2500;
+                    $percent=$dblvlp/$next*100;
+                    $lvlb=3;
+                }else{
+                    $lvl=$ebl4;
+                    $next=2500;
+                    $percent=100;
+                    $lvlb=4;
+                }
+                ?>
+                <style type="text/css">
+                    .LVLB{
+                        width:<?php echo $percent.'%';?>;
+                    }
+                </style>
+        </head>
+            <body id="body">
+            <div id="Main">
+                <i class="iconfont icon-username"></i><?php echo $_SESSION['username']; ?><br>
+                <a href="index.php" class="button button-primary"><i class="iconfont icon-i-back"></i><?=$back?></a><br>
+                <?=$ebcp?>:(<a href="#" onclick="showhelp();">?</a>)：<?php echo $dbpoint;?>
+                <div class="LINEBOX"><div class="PERCENT LVLB"><nobr><center><?php echo $lvl.' '.$dblvlp.'/'.$next;?></center></nobr></div></div>
+    <br>
+    <?php
+    echo $ebde;
+    global $ebch;
+    global $ebcd;
+    function replacement($num,$row){
+        $str=str_replace("1",$GLOBALS['ebch'],$row[(string)$num]);
+        $str=str_replace("0",$GLOBALS['ebcd'],$str);
+        return $str;
     }
-    $sqll='SELECT * FROM point where Username=\''.strtolower($_SESSION['username']).'\'';
-    $rows = $db->query($sqll);
-    if (mysqli_num_rows($rows) < 1){
-        header('Refresh:0;url=firsttime.php');
-        die();
-    }
-    while($row = $rows->fetch_assoc()){
-        $dbpoint=$row['Point'];
-        $dblvlp=$row['lvlp'];
-        $crtt=$row['crtt'];
-    }
-    if($dblvlp<500){
-        $lvl=$ebl1;
-        $next=500;
-        $percent=$dblvlp/$next*100;
-        $lvlb=1;
-    }else if($dblvlp<1500){
-        $lvl=$ebl2;
-        $next=1500;
-        $percent=$dblvlp/$next*100;
-        $lvlb=2;
-    }else if($dblvlp<2500){
-        $lvl=$ebl3;
-        $next=2500;
-        $percent=$dblvlp/$next*100;
-        $lvlb=3;
-    }else{
-        $lvl=$ebl4;
-        $next=2500;
-        $percent=100;
-        $lvlb=4;
+    function showbuy($able2buy,$str){
+        if($able2buy){
+            return $str;
+        }
+        return;
     }
     $rows=null;
     $row=null;
@@ -61,38 +106,28 @@ include './include/'.$lang.'.php';
         $rows = $db->query('SELECT * FROM class where username=\''.strtolower($_SESSION['username']).'\'');
     }
     while($row = $rows->fetch_assoc()){
-        $finaloutput="";
-        $finaloutput=$finaloutput.'你曾经拥有<br>';
+        echo '<table border="1">';
         $times=1;
         while($times<=$classes){
             if($row[(string)$times]==1){
-                $finaloutput=$finaloutput.$truename[$times]." ";
+                $color="#5555CC";
+                $able2buy=false;
+            }else if($lvlb<$lvla[$level[$times]]){
+                $color="#CC5555";
+                $able2buy=false;
+            }else{
+                $color="#55CC55";
+                $able2buy=true;
             }
+            echo '<tr style="background-color:'.$color.';"><td>'.replacement($times,$row).'</td><td>'.$truename[$times].'</td><td>'.ucfirst($level[$times]).'</td><td>'.showbuy($able2buy,'<a href="#" onclick="buy('.$times.','.$cost[$times].');">'.$ebex.'('.$cost[$times].$ebpt.')</a></td></tr>');
             $times++;
         }
-        $finaloutput=$finaloutput.'<br>这只是一个记录，未来这些职业的处理方式如何暂时未知。';
+        echo '</table>';
     }
 ?>
-<html>
-    <head>
-        <link href="css/style.css" rel="stylesheet">
-        <link href="css/all.css" rel="stylesheet">
-        <link href="css/pct.css" rel="stylesheet">
-        <title>Fortress II:Siege</title>
-        <style type="text/css">
-            .LVLB{
-                width: <?=$percent.'%'?>;
-            }
-        </style>
-    </head>
-    <body id="body" class="lazy">
-        <div class="paybox"><!--Main-->
-            <h1>Fortress II:Siege 已终止服务</h1>
-            <div class="LINEBOX"><div class="PERCENT LVLB"><nobr><center><?php echo $lvl.' '.$dblvlp.'/'.$next;?></center></nobr></div></div>
-            <div>你的积分:<?=$dbpoint?></div>
-            <?=$finaloutput?>
-        </div>
-        <div style="color:white;" class="copyright"><p>&nbsp;&nbsp;&nbsp;<?=$copyright?>&copy; 2014-<?php echo date('Y'); ?>.TS Studio <?=$alrr?> 吉ICP备17003700号</p></div>
-    </body>
-    <script src="./css/bgi.js"></script>
+</div>
+<div style="color:white;" class="copyright"><p>&nbsp;&nbsp;&nbsp;<?=$copyright?>&copy; 2014-<?php
+echo date('Y'); ?>.TS Studio <?=$alrr?> 吉ICP备17003700号</p></div>
+</body>
+<script defer src="./css/bgi.js"></script>
 </html>
---
+++ pay.php
@@ -0,0 +1,115 @@
+<?php session_start();
+error_reporting(E_ALL || ~E_NOTICE);
+include './include/server-info.php';
+include './include/classes.php';
+$lang=$_SESSION['language'];
+include './include/'.$lang.'.php';
+if(!isset($_GET['class'])){
+    header('Refresh:0;url=ebw.php');
+    die();
+}
+if ($_SESSION['username']==""){
+    echo "You are not logging in,jumping to the log-in page.";
+    header('Refresh:0;url=loginform.php?URL=login.php&code=105');
+    die();
+}
+$username=strtolower($_SESSION['username']);
+$class=$_GET['class'];
+if(!is_numeric($class)){
+    die("错误，请返回");
+}else if($class>$classes){
+    die("错误，请返回");
+}
+$value=$cost[$class];
+$con=new mysqli($dbhost,$dbuser,$dbpawd,$dbname);
+$dbusername=null;
+$dbjob=null;
+$result = $con->query( "select username,`{$class}` from class where username ='{$username}';" ); 
+while ($row=mysqli_fetch_array($result)) {//while循环将$result中的结果找出来 
+    $dbusername=$row["username"]; 
+    $dbjob=$row[$class]; 
+}
+if ($dbjob==1){
+    //如果拥有职业
+    $owned=true;
+}
+$result=null;
+//检测职业积分是否够，等级积分是否够
+$result = $con->query( "select * from point where Username ='{$username}';" ); 
+while ($row=mysqli_fetch_array($result)) {//while循环将$result中的结果找出来 
+    $dbusername=$row["Username"]; 
+    $dbpoint=$row["Point"]; 
+    $dblvlp=$row["lvlp"];
+}
+if($dblvlp<500){
+    $lvlb=1;
+}else if($dblvlp<1500){
+    $lvlb=2;
+}else if($dblvlp<2500){
+    $lvlb=3;
+}else{
+    $lvlb=4;
+}
+$classlvl=$level[$class];
+$canbuy=true;
+if($lvlb<$lvla[$classlvl]){//等级不够
+    $canbuy=false;
+    $show=$lvtl;
+    $chkidshow=$egcl;
+}else if($dbpoint<$cost[$class]){//积分不够
+    $canbuy=false;
+    $show=$nept;
+    $chkidshow=$egcl;
+}else if($owned){//拥有职业
+    $canbuy=false;
+    $show=$ebch;
+    $chkidshow=$egcl;
+}else{
+    $canbuy=true;
+    //可以购买，则
+    //生成购买信息
+    $con->query("insert into payment (username,value,obj) values ('{$username}','{$value}','{$class}')");
+    $result=$con->query("select chkid from payment where username='{$username}' and status=0");
+    while ($row=mysqli_fetch_array($result)) {//while循环将$result中的结果找出来 
+        $chkid=$row["chkid"];
+    }
+    //订单号格式化
+    $strinneed="100000000000000000000";
+    $outs=$strinneed.$chkid;
+    $chkidshow=substr($outs, -20);
+}
+//删除未完成订单
+$con->query("delete from payment where username='{$username}' and status=0");
+if($canbuy){
+    $_SESSION["CHECKID"]=$chkid;
+    $chkbutt='<a href="exchange.php" class="button button-pill button-primary">'.$chec.'</a>';
+}else{
+    $chkid=$egcl;
+    $chkbutt='<a href="#" class="button button-pill button-tiny">'.$show.'</a>';
+}
+?>
+
+<html>
+    <head>
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/all.css" rel="stylesheet">
+        <title><?=$cash?></title>
+    </head>
+    <body id="body" class="lazy">
+        <div class="paybox"><!--Main-->
+            <h1><?=$cash?></h1>
+            <h2><?=$objb?></h2>
+            <?=$f2sj?><?=$truename[$class]?>
+            <div class="deti">
+                <?=$poin?><?=$dbpoint?><br>
+                <?=$pcos?><?=$cost[$class]?><br>
+                <?=$pres?><?=$dbpoint-$cost[$class]?><br>
+                <?=$chid?><?=$chkidshow?><br>
+                <!--结算按钮-->
+                <?=$chkbutt?>
+            </div>
+        </div>
+        <div style="color:white;" class="copyright"><p>&nbsp;&nbsp;&nbsp;<?=$copyright?>&copy; 2014-<?php echo date('Y'); ?>.TS Studio <?=$alrr?> 吉ICP备17003700号</p></div>
+    </body>
+    <script src="./css/bgi.js"></script>
+</html> 
